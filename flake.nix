568b430a5d5f7c222d4e16fdc2f37195ed6dd643
Adam Rizkalla <adam@saronic.com>
2025-04-01 05:32:54 +0000

    feat: add test module to check for unsupported json references in foxglove schema
    Signed-off-by: Adam Rizkalla <adam@saronic.com>

--- flake.nix
+++ flake.nix
@@ -3,8 +3,12 @@
 
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
@@ -19,8 +23,6 @@
       {
         devShells.default = mkShell {
           nativeBuildInputs = [
-            can-utils
-            openssl
             rust-bin.nightly.latest.default
           ];
         };
--- foxglove_derive/src/lib.rs
+++ foxglove_derive/src/lib.rs
@@ -1,21 +1,40 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Foxglove)]
 pub fn foxglove(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
+    let test_mod = format_ident!("test_{}", name);
+    let test_fn = format_ident!("check_schema_refs_{}", name);
     let expanded = quote! {
         impl Foxglove for #name {
             fn to_jsonschema() -> String {
-                serde_json::to_string(&schema_for!(#name)).unwrap()
+                serde_json::to_string(&schemars::schema_for!(#name)).unwrap()
             }
             fn to_foxglove_schema() -> String {
                 String::from(stringify!(#name))
             }
             fn to_foxglove(&self) -> String {
                 serde_json::to_string(&self).unwrap()
+            }
+        }
+
+        // Set up a test case to ensure schema does not contain any JSON references.
+        // See: https://docs.foxglove.dev/docs/connecting-to-data/frameworks/custom#json
+        #[cfg(test)]
+        #[allow(non_snake_case)]
+        mod #test_mod {
+            use super::#name;
+
+            #[test]
+            #[allow(non_snake_case)]
+            fn #test_fn() {
+                let schema = schemars::schema_for!(#name);
+                let schema_json_str = serde_json::to_string(&schema).unwrap();
+                eprintln!("{}", serde_json::to_string_pretty(&schema).unwrap());
+                assert!(!schema_json_str.contains("$ref"));
             }
         }
     };
